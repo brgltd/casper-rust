ca97d0b2621c24997e6a5b486d8ecf230cb83e65
brgltd <brgltdxyz@gmail.com>
2022-04-04 09:07:51 -0300

    feat: add basic icons for topbar

--- components/topbar/topbar.tsx
+++ components/topbar/topbar.tsx
@@ -1,15 +1,27 @@
+import { MdDarkMode, MdShare } from "react-icons/md";
+import { AiFillGithub, AiOutlineRead } from "react-icons/ai";
+import Logo from "../../assets/svg-jsx/logo";
 import styles from "./topbar.module.css";
 
 export default function Topbar() {
   return (
     <nav className={styles.nav}>
       <ul className={styles.list}>
-        <li>home</li>
-        <li>content</li>
-        <li>share</li>
-        <li>repo</li>
-        <li>network</li>
-        <li>docs</li>
+        <li>
+          <Logo />
+        </li>
+        <li>
+          <AiFillGithub />
+        </li>
+        <li>
+          <AiOutlineRead />
+        </li>
+        <li>
+          <MdShare />
+        </li>
+        <li>
+          <MdDarkMode />
+        </li>
       </ul>
     </nav>
   );
