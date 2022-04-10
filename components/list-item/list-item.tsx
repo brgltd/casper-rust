008a4148fd612dbe78fb917422ec6e336bcddac5
brgltd <brgltdxyz@gmail.com>
2022-04-10 14:01:05 -0300

    feat: improve list-item background and border

--- components/list-item/list-item.tsx
+++ components/list-item/list-item.tsx
@@ -1,8 +1,9 @@
-import IndexData from "../../types/index-data";
+import Link from "next/link";
 import { BiRun } from "react-icons/bi";
 import { GiStrong } from "react-icons/gi";
 import { GiStrongMan } from "react-icons/gi";
 import { RiTodoLine } from "react-icons/ri";
+import IndexData from "../../types/index-data";
 import styles from "./list-item.module.css";
 
 export default function ListItem({
@@ -12,28 +13,32 @@ export default function ListItem({
   estimation,
 }: IndexData) {
   return (
-    <li className={styles.item}>
-      <div className={styles.info}>
-        <div className={styles.id}>{id}</div>
-        <div className={styles.experience}>{experience}</div>
-        <div className={styles.iconWrapper}>
-          {experience === "Beginner" ? (
-            <BiRun />
-          ) : experience === "Advanced" ? (
-            <GiStrongMan />
-          ) : (
-            <GiStrong />
-          )}
-        </div>
-      </div>
-      <div>{title}</div>
-      <div className={styles.status}>
-        <div className={styles.iconWrapper}>
-          <RiTodoLine />
-        </div>
-      </div>
-      <div className={styles.start}>start now</div>
-      <div className={styles.estimation}>Est. {estimation}</div>
+    <li>
+      <Link href={`/levels/${id}`}>
+        <a className={styles.item}>
+          <div className={styles.info}>
+            <div className={styles.id}>{id}</div>
+            <div className={styles.experience}>{experience}</div>
+            <div className={styles.iconWrapper}>
+              {experience === "Beginner" ? (
+                <BiRun />
+              ) : experience === "Advanced" ? (
+                <GiStrongMan />
+              ) : (
+                <GiStrong />
+              )}
+            </div>
+          </div>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.status}>
+            <div className={styles.iconWrapper}>
+              <RiTodoLine />
+            </div>
+          </div>
+          <div className={styles.start}>start now</div>
+          <div className={styles.estimation}>Est. {estimation}</div>
+        </a>
+      </Link>
     </li>
   );
 }
