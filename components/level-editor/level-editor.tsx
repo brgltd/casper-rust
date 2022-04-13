8655b735df9d7f679fb156c02823d7854111555e
brgltd <brgltdxyz@gmail.com>
2022-04-13 08:32:57 -0300

    feat: set rust indentation to 4

--- components/level-editor/level-editor.tsx
+++ components/level-editor/level-editor.tsx
@@ -24,6 +24,7 @@ export default function LevelEditor({
         options={{
           wordWrap: "on",
           minimap: { enabled: false },
+          tabSize: 4,
         }}
       />
     </div>
